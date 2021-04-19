94808a4ea6af7820d18f970dbc74411c67b23866
Jaderson Alex Da Costa Silva <jadersonalex@gmail.com>
2021-04-19 00:35:41 -0300

    update

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public void Pausado()
+    {
+        Time.timeScale = 0; // Pausado
+    }
+
+    public void SairDoPause()
+    {
+        Time.timeScale = 1; // sair do pause
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Fox (Unity versão  2020.1.2f1)
+Projeto da raposa
+
+
+![333](https://user-images.githubusercontent.com/15167847/103813588-188d3080-503f-11eb-9157-d3a162c5bf57.gif)
